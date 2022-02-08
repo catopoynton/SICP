bfe39363eb98fd6bf295a4025e46c5d1fb239dba
Cato Poynton <catopoynton@Catos-MacBook-Pro.local>
2022-02-09 08:32:03 +1100

    continuing on four queens

--- Chapter 2/2.42.scm
+++ Chapter 2/2.42.scm
@@ -1,3 +1,7 @@
+(load "/Users/catopoynton/Desktop/SICP/Chapter 2/accumulate.scm")
+(load "/Users/catopoynton/Desktop/SICP/Chapter 2/2.39.scm")
+
+
 (define (queens board-size) 
     (define (queen-cols k)
         (if (= k 0)
@@ -12,20 +16,38 @@
 
 (define (adjoin-position new-row k rest-of-queens)
 ;for each board in rest of queens the kth item of that board needs to have the value of new row
-    (map (lambda (col) (append col (list new-row))) rest-of-queens))
+    (map (lambda (col) (update col new-row k)) rest-of-queens))
 
 ;function to replace the value of a list at an index
-(define (update l1 item idx )
-    (define (update-iter head tail pos)
-        (if (= pos idx)
-            (append (append (list item) head) (cdr tail))
-        (update-iter (append (list (car tail)) head) (cdr tail) (+ pos 1))))
-    (update-iter '() l1 0))
+(define (update lst val idx )
+    (if (null? lst)
+        lst
+        (cons
+            (if (= 0 idx)
+                val
+                (car lst))
+                (update (cdr lst) val (- idx 1)))))
 
+;maps each board in "rest-of-queens" to a set of boards, where each board has a new row added to column k
 (define (add-row rest-of-queens k board-size)
-    (map (lambda (new-row)
+    (flatmap (lambda (new-row)
             (adjoin-position new-row k rest-of-queens)) (enumerate-interval 1 board-size)))
 
-(display (update (list "dog" "bumbi" 1) "cat" 0))
+;returns an empty board represented as a list with each item in the list = -1
+(define (empty-board board-size)
+    (define (board-iter board length)
+        (if (= 0 length)
+            board
+        (board-iter (append (list -1) board) (- length 1))))
+    (board-iter (list) board-size))
+
+(define (enumerate-interval i j)
+    (define (iter result j)
+        (if (> i j)
+            result
+        (iter (cons j result) (- j 1))))
+    (iter '() j))
+        
+(display (add-row (list (empty-board 3) (empty-board 3)) 2 3))
 
 
